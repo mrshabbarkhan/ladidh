30ed5857a6234c017a3cad1edab6292ab2a7b62f
shabbar khan <shabbarkhan.js@gmail.com>
2024-08-20 12:15:56 +0530

    font and sizing

--- src/components/CheckOutFooter.jsx
+++ src/components/CheckOutFooter.jsx
@@ -1,18 +1,18 @@
 function CheckOutFooter() {
-    return (
-      <div className="flex bg-primary items-center justify-between px-4 py-2 text-white my-2 mx-2 rounded-lg">
-        <div className="flex gap-2">
-          <h1>3 items |</h1>
-          <h1 className="font-semibold">&#x20B9;930</h1>
-        </div>
-        <div className="flex gap-2 items-center">
-          <span className="text-lg">Checkout</span>
-          <span>
-            <i className="fa fa-angle-right"></i>
-          </span>
-        </div>
+  return (
+    <div className="flex bg-primary items-center justify-between px-4 py-2 text-white my-2 mx-2 rounded-lg">
+      <div className="flex gap-2">
+        <h1>3 items |</h1>
+        <h1 className="font-semibold">&#x20B9;930</h1>
       </div>
-    );
+      <div className="flex gap-2 items-center">
+        <span className="text-sm">Checkout</span>
+        <span>
+          <i className="fa fa-angle-right"></i>
+        </span>
+      </div>
+    </div>
+  );
 }
 
-export default CheckOutFooter
+export default CheckOutFooter;
--- src/features/Cart/Card.jsx
+++ src/features/Cart/Card.jsx
@@ -1,6 +1,6 @@
 function Card({ img, pack, tittle, discount, code, price, oldPrice }) {
   return (
-    <section className="max-w-[15rem] min-w-[15rem] mb-3 text-wrap rounded-xl pb-2 bg-white shadow-lg">
+    <section className="max-w-[16rem] min-w-[16rem] mb-3 text-wrap rounded-xl p-4 pb-2 bg-white shadow-lg">
       <img
         src={img}
         alt={img}
@@ -8,16 +8,24 @@ function Card({ img, pack, tittle, discount, code, price, oldPrice }) {
       />
       <div className="px-2 mt-1">
         <p className="text-xs font-medium text-primary-dark">{pack}</p>
-        <h1 className="mt-2 text-md text-gray-800 leading-6 font-medium">{tittle}</h1>
-        {discount&&<p className="text-xs mt-2 text-orange-500">
-          FLAT {discount} off Code: {code}
-        </p>}
+        <h1 className="mt-2 text-md text-gray-800 leading-6 font-medium">
+          {tittle}
+        </h1>
+        {discount && (
+          <p className="text-xs mt-2 text-orange-500">
+            FLAT {discount} off Code: {code}
+          </p>
+        )}
         <div className="flex justify-between items-center mt-2">
           <span className="flex gap-2 items-center">
-            <h1 className="text-xl text-primary-dark font-semibold">&#x20B9; {price}</h1>
-            <span className="line-through text-gray-500 font-medium">{oldPrice}</span>
+            <h1 className="text-xl text-primary-dark font-semibold">
+              &#x20B9; {price}
+            </h1>
+            <span className="line-through text-gray-500 font-medium">
+              {oldPrice}
+            </span>
           </span>
-          <button className="border border-primary-dark px-2 rounded-lg hover:text-white hover:bg-primary-dark transition">
+          <button className="border border-primary-dark text-sm font-semibold px-2 rounded-lg hover:text-white hover:bg-primary-dark transition">
             Add
           </button>
         </div>
