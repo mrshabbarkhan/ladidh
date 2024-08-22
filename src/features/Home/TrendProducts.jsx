b3cdd25156511490c821dedee09d1d90e365fa00
Shivam Goswami <shivamgoswami.work@gmail.com>
2024-08-22 12:14:00 +0530

    decrease mb-6 TrendPro

--- src/features/Home/TrendProducts.jsx
+++ src/features/Home/TrendProducts.jsx
@@ -4,7 +4,7 @@ import Card from "../Cart/Card"
 function TrendProducts() {
       const navigateToProductList = "/product-details";
     return (
-        <section className="mb-20">
+        <section className="mb-12">
             <h1 className="text-lg font-medium mb-3 mt-2">Product on Trend</h1>
             <div className="flex gap-5 overflow-x-auto w-full ">
                 {Tranding_Products.map((dts,index) => {
