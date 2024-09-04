aebc9c764fae4e0460766bcf1c6ab810d410b953
Shivam Goswami <shivamgoswami.work@gmail.com>
2024-09-04 20:15:02 +0530

    add banners to home

--- src/features/Home/OfferForYou.jsx
+++ src/features/Home/OfferForYou.jsx
@@ -1,16 +1,18 @@
+import { useSelector } from "react-redux";
 import { OfferBanners } from "../../utils/utils";
 
 function OfferForYou() {
-    return (
-      <section className="mt-8 overflow-hidden">
-        <h1 className="font-medium text-lg">Offer for you</h1>
-        <div className="offer_img h-36 md:h-40 flex  gap-x-5 mt-2 overflow-auto">
-          {OfferBanners.map((bner,index) => {
-           return <img key={index} src={bner} alt={bner} />
-         })}
-        </div>
-      </section>
-    );
+  const { banners } = useSelector((state) => state.adminDashboard);
+  return (
+    <section className="mt-8 overflow-hidden">
+      <h1 className="font-medium text-lg">Offer for you</h1>
+      <div className="offer_img h-36 md:h-40 flex  gap-x-5 mt-2 overflow-auto">
+        {banners.map((bner, index) => {
+          return <img key={index} src={bner.Img} alt={bner} />;
+        })}
+      </div>
+    </section>
+  );
 }
 
-export default OfferForYou
+export default OfferForYou;
