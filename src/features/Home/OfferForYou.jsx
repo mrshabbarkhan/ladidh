e01db41378bfd37b51bf371d251041e87d34c3c9
Shivam Goswami <shivamgoswami.work@gmail.com>
2024-09-06 17:39:23 +0530

    test offerPage

--- src/features/Home/OfferForYou.jsx
+++ src/features/Home/OfferForYou.jsx
@@ -1,12 +1,12 @@
 import { useSelector } from "react-redux";
 
 function OfferForYou() {
-  const { banners } = useSelector((state) => state.adminDashboard);
+  const { banners=[] } = useSelector((state) => state.adminDashboard);
   return (
     <section className="mt-8 overflow-hidden">
       <h1 className="font-medium text-lg">Offer for you</h1>
       <div className="offer_img h-36 md:h-40 flex  gap-x-5 mt-2 overflow-auto">
-        {banners.map((bner, index) => {
+        {banners?.map((bner, index) => {
           return <img key={index} src={bner.Img} alt={bner} />;
         })}
       </div>
